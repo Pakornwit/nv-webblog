939b228f1aad4ee15516435c892aba8c26c2dbdc
Pakornwit <pakornwitj62@nu.ac.th>
2021-08-06 18:34:06 +0700

    new

--- client/src/components/index.vue
+++ client/src/components/index.vue
@@ -2,7 +2,13 @@
     <h1>Get All Users</h1>
 </template>
 <script>
+import UserService from '@/services/UsersSercice'
+
 export default {
+    async created () {
+        let results = (await UserService.index()).data
+        console.log(results)
+    } 
     
 }
 </script>
