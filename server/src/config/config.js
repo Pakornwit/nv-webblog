e734f9d1fc31e820bbdfacfb442fa95e6c5ebb6a
Pakornwit <pakornwitj62@nu.ac.th>
2021-09-05 01:56:49 +0700

    new 5/9/64

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  port: 8081,
   db: {
     database: process.env.DB_NAME || 'nvWebblogDb',
     user: process.env.DB_User || 'root',
@@ -6,6 +7,9 @@ module.exports = {
     options: {
       dialect: process.env.DIALECT || 'sqlite',
       storage: './nvwebblog-db.sqlite'      
-    },    
+    },  
+  },  
+    authentication:{
+      jwtSecret: "test"
+    }
   }
-}
